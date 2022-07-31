92e161600de3411803b83e5ba02af893b0227882
Canberk Aydemir <canberk.aydemir@yahoo.com>
2022-07-31 20:33:55 +0300

    Implement Axios http request

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export async  function getUserDetails(userId){
+  try {
+
+    const user = await axios("https://jsonplaceholder.typicode.com/users/"+userId);
+    const post = await axios("https://jsonplaceholder.typicode.com/posts/"+userId);
+    
+    user.data.posts = post.data;
+
+    console.log("User:",user.data)
+
+} catch (e) {
+    console.log(e);
+}
+};
